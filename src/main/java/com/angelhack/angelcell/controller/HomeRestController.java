79d56fd5e59652cd0de2bdb37f5ecf5e9a720009
yunjey0 <sehajyang@gmail.com>
2019-06-02 00:02:11 +0900

    Dev : Post request 개발중

--- src/main/java/com/angelhack/angelcell/controller/HomeRestController.java
+++ src/main/java/com/angelhack/angelcell/controller/HomeRestController.java
@@ -6,8 +6,13 @@ import com.angelhack.angelcell.dto.MessageSaveDto;
 import com.angelhack.angelcell.dto.MessageSendDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 /**
  * @author seha
@@ -31,9 +36,15 @@ public class HomeRestController {
     }
 
     @PostMapping("/users/{num}/send")
-    public int sendMessage(@PathVariable("num")Long num){
+    public int sendMessage(@PathVariable("num")Long num) throws IOException {
         int groupCount = homeService.getUserCountByGroupId(num);
         log.debug("$$$"+groupCount);
+        HttpConnection.Response rs = (HttpConnection.Response) Jsoup
+                .connect("https://192.168.0.152")
+                .data("I_D", "a")
+                .data("P_W", "b")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
+                .method(Connection.Method.POST).execute();
         return 0;
     }
 }
